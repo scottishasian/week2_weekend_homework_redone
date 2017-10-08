50d1217ca8736b7d2ec34a651f486f1e4da8d8d4
Kynan <k.song@blueyonder.co.uk>
2017-10-08 23:10:22 +0100

    Passed test add_rooms and show_rooms.

--- command.rb
+++ command.rb
@@ -4,6 +4,25 @@ require_relative( 'songs.rb' )
 
 class Command
 
+  attr_accessor :rooms
+
+  def initialize()
+    @rooms = []
+  end
+
+  def show_rooms
+    if @rooms == []
+      return "No rooms built"
+    else
+      return @rooms
+    end
+  end
+
+  def add_rooms(room_details)
+    @rooms << room_details
+    return show_rooms
+  end
+
 end
 
 # Creates new room, guest and song hashes to be added.
--- specs/command_spec.rb
+++ specs/command_spec.rb
@@ -7,4 +7,21 @@ require( 'minitest/rg' )
 
 class TestCommand < MiniTest::Test
 
+  def setup
+    @command = Command.new()
+    @room = Room.new(name:"Singing Room", cost: 400, capacity: 2)
+  end
+
+  def test_show_rooms_empty
+    assert_equal("No rooms built", @command.show_rooms)
+  end
+
+  def test_add_rooms
+    @command.add_rooms(@room)
+    rooms = []
+    rooms << @room
+    assert_equal(rooms, @command.show_rooms)
+  end
+
+
 end
